4534c3dae43b738bf65ac2f4a9f45d1d2f99952f
sivasrinilab <88289718+sivasrinilab@users.noreply.github.com>
2023-08-10 12:57:39 +0530

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -21,7 +21,7 @@ function currentDiv(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("demodots");
+ /* var dots = document.getElementsByClassName("demodots");
   if (n > x.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = x.length} ;
   for (i = 0; i < x.length; i++) {
@@ -31,5 +31,5 @@ function showDivs(n) {
     dots[i].className = dots[i].className.replace(" w3-white", "");
   }
   x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
+  dots[slideIndex-1].className += " w3-white";*/
 }
