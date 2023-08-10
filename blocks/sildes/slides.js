8f69aea1db363edb4022c6e198fa5155cb06a1a7
sivasrinilab <88289718+sivasrinilab@users.noreply.github.com>
2023-08-10 13:01:34 +0530

    Update slides.js

--- blocks/sildes/slides.js
+++ blocks/sildes/slides.js
@@ -1,5 +1,6 @@
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 export default function decorate(block) {
+   alert('hellooo')
    [...block.children].forEach((row) => {
       alert('hi' + row);
      row.addClass('click');
