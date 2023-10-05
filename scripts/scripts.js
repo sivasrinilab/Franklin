3cd6f6aab90ab20f40d2e025dfd448f0b0fe1a36
sivasrinilab <88289718+sivasrinilab@users.noreply.github.com>
2023-10-05 21:04:14 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -164,6 +164,21 @@ listItems.forEach((item) => {
   }
 
 });
+  $(".accordion-wrapper .item-content:not(:first)").css("display", "none");
+	$(".accordion-wrapper .item-title").click(function() {
+		var isOpen = $(this).hasClass("open");
+		$(".accordion-wrapper .item-title").removeClass("open");
+		if (!isOpen) {
+			$(this).removeClass("open");
+		} else {
+			$(this).addClass("open");
+		}
+		//$(this).toggleClass("open");
+		$(this).next(".item-content").css("display", function(_, value) {
+			return value === "none" ? "block" : "none";
+		});
+		$(".accordion-wrapper .item-content").not($(this).next()).css("display", "none");
+	});
   $('.slides').slick();
 
 });
