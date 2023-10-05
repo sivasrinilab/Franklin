1d891bdf674d32b7885180e255b0ab40eef80660
sivasrinilab <88289718+sivasrinilab@users.noreply.github.com>
2023-10-05 19:57:18 +0530

    Update accordion.js

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -20,21 +20,3 @@ export default function init(el) {
 		document.querySelector('body > main > div.section.accordion-container .accordion-wrapper .item-title').classList.add('open');
 
 }
-
-$(document).ready(function() {
-	$(".accordion-wrapper .item-content:not(:first)").css("display", "none");
-	$(".accordion-wrapper .item-title").click(function() {
-		var isOpen = $(this).hasClass("open");
-		$(".accordion-wrapper .item-title").removeClass("open");
-		if (!isOpen) {
-			$(this).removeClass("open");
-		} else {
-			$(this).addClass("open");
-		}
-		//$(this).toggleClass("open");
-		$(this).next(".item-content").css("display", function(_, value) {
-			return value === "none" ? "block" : "none";
-		});
-		$(".accordion-wrapper .item-content").not($(this).next()).css("display", "none");
-	});
-});
