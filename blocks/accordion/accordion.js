56f8bdb66b07da72284b2a395fc400ac81eda19a
sivasrinilab <88289718+sivasrinilab@users.noreply.github.com>
2023-10-06 22:42:59 +0530

    Update accordion.js

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,26 +1,39 @@
 export default function init(el) {
-  const titles = el.querySelectorAll(':scope > div:nth-child(odd)');
-  titles.forEach((title) => {
-    // Add a class to the title container
-    title.classList.add('item-title');
-    // Add a class to the content
-    title.nextElementSibling.classList.add('item-content');
-    // Add a click handler to open/close the content
-    title.addEventListener('click', () => {
-      // Toggle the 'open' class for the clicked title
-      title.classList.toggle('open');
-      // Close other open accordions
-      titles.forEach((otherTitle) => {
-        if (otherTitle !== title) {
-          otherTitle.classList.remove('open');
+    const titles = el.querySelectorAll(':scope > div:nth-child(odd)');
+    
+    titles.forEach((title) => {
+        // Add a class to the title container
+        title.classList.add('item-title');
+        
+        // Remove the empty div (if necessary)
+        // title.querySelector(':scope > div:last-of-type').remove();
+        
+        // Add a class to the content
+        const content = title.nextElementSibling;
+        content.classList.add('item-content');
+        
+        // Add a click handler to open/close the content
+        title.addEventListener('click', () => {
+            // Close all other accordion sections
+            titles.forEach((otherTitle) => {
+                if (otherTitle !== title) {
+                    otherTitle.classList.remove('open');
+                    otherTitle.nextElementSibling.style.maxHeight = '0';
+                }
+            });
+            
+            // Toggle the open class and expand/collapse the content
+            title.classList.toggle('open');
+            if (title.classList.contains('open')) {
+                content.style.maxHeight = content.scrollHeight + 'px';
+            } else {
+                content.style.maxHeight = '0';
+            }
+        });
+        
+        // Check if this accordion item should be initially open
+        if (title.classList.contains('open')) {
+            content.style.maxHeight = content.scrollHeight + 'px';
         }
-      });
     });
-  });
-
-  //accordion
-  const activeAccordion = document.querySelector('body > main > div.section.accordion-container .accordion-wrapper .item-title');
-  if (activeAccordion !== null) {
-    activeAccordion.classList.add('open');
-  }
 }
