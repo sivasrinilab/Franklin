3b5f5abe9b4333e82d17be08caa70ba0ae9a161f
sivasrinilab <88289718+sivasrinilab@users.noreply.github.com>
2023-10-06 13:40:21 +0530

    Update accordion.js

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,71 +1,22 @@
-
-
 export default function init(el) {
+    const titles = el.querySelectorAll(':scope > div:nth-child(odd)');
+    titles.forEach((title) => {
+      // Add a class to the title container
+      title.classList.add('item-title');
+      // Remove the empty div
+     // title.querySelector(':scope > div:last-of-type').remove();
+      // Add a class to the content
+      title.nextElementSibling.classList.add('item-content');
+      // Add a click handler to open the content
+      title.addEventListener('click', () => {
+        title.classList.toggle('open');
+      });
+        // Select the source element
 
-	const titles = el.querySelectorAll(':scope > div:nth-child(odd)');
+    });
+    //accordion
+var activeAccordion = document.querySelector('body > main > div.section.accordion-container .accordion-wrapper .item-title');
+if(activeAccordion !== null)
+    document.querySelector('body > main > div.section.accordion-container .accordion-wrapper .item-title').classList.add('open');
 
-	titles.forEach((title) => {
-
-		// Add a class to the title container
-
-		title.classList.add('item-title');
-
-		// Remove the empty div
-
-		// title.querySelector(':scope > div:last-of-type').remove();
-
-		// Add a class to the content
-
-		title.nextElementSibling.classList.add('item-content');
-
-		// Add a click handler to open the content
-
-		$(".accordion-wrapper .item-content:not(:first)").css("display", "none");
-
-		title.addEventListener('click', (event) => {
-
-			//title.classList.toggle('open');
-
-			var isOpen = $(this).hasClass("open");
-
-			$(".accordion-wrapper .item-title").removeClass("open");
-
-			if (!isOpen) {
-				$(event.target.closest('.item-title')).addClass("open");
-				
-
-			} else {
-
-				$(event.target.closest('.item-title')).removeClass("open");
-
-			}
-
-			//$(this).toggleClass("open");
-
-			$(event.target.closest('.item-content')).css("display", function(_, value) {
-
-				return value === "none" ? "block" : "none";
-
-			});
-
-			$(".accordion-wrapper .item-content").not($(this).next()).css("display", "none");
-
-		});
-
-		// Select the source element
-
- 
-
-	});
-
-	//accordion
-
-	var activeAccordion = document.querySelector('body > main > div.section.accordion-container .accordion-wrapper .item-title');
-
-	if (activeAccordion !== null)
-
-		document.querySelector('body > main > div.section.accordion-container .accordion-wrapper .item-title').classList.add('open');
-
- 
-
-}
+  }
