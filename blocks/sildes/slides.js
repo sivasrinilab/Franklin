07b342e68bfd07705fd1e353a6dc70b8380b1a54
sivasrinilab <88289718+sivasrinilab@users.noreply.github.com>
2023-10-09 20:43:48 +0530

    Update slides.js

--- blocks/sildes/slides.js
+++ blocks/sildes/slides.js
@@ -1,6 +1,3 @@
-//import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
-//<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
-
 /*
      _ _      _       _
  ___| (_) ___| | __  (_)___
