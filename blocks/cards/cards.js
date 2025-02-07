0f5af44d11508e9d5705c83d0e4fb4c9fb003bb2
sivasrinilab <88289718+sivasrinilab@users.noreply.github.com>
2025-02-07 21:04:18 +0530

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -10,7 +10,7 @@ export default function decorate(block) {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
-    ul.append(li);
+    ul.append(li);    
   });
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
@@ -36,9 +36,36 @@ if (strongTag) {
 
 var cards=document.querySelectorAll(".section.cards-container")
 if(cards){
-    cards[0].classList.add("card-details")
-    cards[1].classList.add("card-details")
+   // cards[0].classList.add("card-details")
+    cards[3].classList.add("card-details")
+    cards[2].classList.add("card-details")
 }
+document.querySelector("main > div:nth-child(7)").classList.add('large-cards');
+document.querySelector("main > div:nth-child(9)").classList.add('large-cards');
+setTimeout(function() {
+  document.querySelectorAll(".large-cards ul li").forEach((ele) => {
+    ele.querySelector("div:last-child").classList.add('card-highlight');
+    ele.querySelector("div:nth-child(2)").classList.add('card-middle');
+  }); 
+}, 200);
+setTimeout(function() {
+  let liEle = document.querySelectorAll(".large-cards ul li"); 
+  let paraLen =  document.querySelectorAll(".large-cards ul li:first-child .cards-card-image > p").length;
+  if(paraLen < 2) {
+    liEle.forEach((li) => {
+      let cardBtn = li.querySelector("div:last-child p:nth-child(1)").innerHTML;
+      let cardRating = li.querySelector("div:last-child p:nth-child(2)").innerHTML;
+      let imgEle = li.querySelector(".cards-card-image");
+      let btnP = document.createElement('p');
+      let logoP = document.createElement('p');
+      logoP.innerHTML = cardRating;
+      btnP.innerHTML = cardBtn;
+      console.log(btnP);
+      imgEle.append(logoP)
+      imgEle.append(btnP);
+    });  
+  }
+}, 500);
 
-
+  
 }
