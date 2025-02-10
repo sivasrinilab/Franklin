7a5dc50a8da072c3e0566d80da45b2cdc7cdc652
sivasrinilab <88289718+sivasrinilab@users.noreply.github.com>
2025-02-10 23:59:04 +0530

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,1 +1,13 @@
 
+const addEventListener = () => {
+    const heroH1 = document.querySelector("#credit-cards");
+    const nextSibling = document.querySelector("body > main > div:nth-child(2)");
+    const nextSiblingWrapper = document.querySelector("body > main > div:nth-child(2) > div.default-content-wrapper");
+    const heroPicure = document.querySelector("body > main > div.section.hero-container > div > div > div > div > picture");
+    nextSiblingWrapper.prepend(heroH1);
+    const clones = nextSibling.cloneNode(true);
+    heroPicure.after(clones);
+    nextSibling.remove();
+};
+
+document.addEventListener('DOMContentLoaded', addEventListener());
